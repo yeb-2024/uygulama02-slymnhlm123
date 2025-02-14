faaba6627eef4e1df0f5cf6c44ede8824bd5ae9c
slymnhlm123 <142054022+slymnhlm123@users.noreply.github.com>
2025-02-14 18:05:49 +0300

    Kullanıcı şifre giriş kısmı oluşturuldu

--- JText/src/main/java/org/example/GirişSayfası.java
+++ JText/src/main/java/org/example/GirişSayfası.java
@@ -8,17 +8,30 @@ public class GirişSayfası
     JFrame frame = new JFrame();
     JTextField kullanıcı = new JTextField();
 
+    JTextField sifre = new JTextField();
+
 
     public  GirişSayfası()
     {
         JPanel panelust = new JPanel();
         panelust.setLayout(new FlowLayout());
 
+
         JLabel üstlabel = new JLabel("Kullanıcı Adı");
         kullanıcı.setPreferredSize(new Dimension(200,40));
 
+        JPanel panelorta = new JPanel();
+        panelust.setLayout(new FlowLayout());
+
+
+        JLabel ortalabel = new JLabel("Şifre");
+        sifre.setPreferredSize(new Dimension(200,40));
+
         panelust.add(üstlabel);
         panelust.add(kullanıcı);
+
+        panelorta.add(ortalabel);
+        panelorta.add(sifre);
 
         frame.setTitle("Giriş Sayfası");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -26,6 +39,7 @@ public class GirişSayfası
         frame.setLayout(new GridLayout(3,1));
 
         frame.add(panelust);
+        frame.add(panelorta);
         frame.setVisible(true);
     }
 }
