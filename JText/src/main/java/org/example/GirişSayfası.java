ae3751969a0ec7e976a21ec569e76fa8c5445b6d
slymnhlm123 <142054022+slymnhlm123@users.noreply.github.com>
2025-02-14 18:02:51 +0300

    Kullanıcı adı giriş kısmı oluşturuldu

--- JText/src/main/java/org/example/GirişSayfası.java
+++ JText/src/main/java/org/example/GirişSayfası.java
@@ -6,13 +6,26 @@ import java.awt.*;
 public class GirişSayfası
 {
     JFrame frame = new JFrame();
+    JTextField kullanıcı = new JTextField();
+
 
     public  GirişSayfası()
     {
+        JPanel panelust = new JPanel();
+        panelust.setLayout(new FlowLayout());
+
+        JLabel üstlabel = new JLabel("Kullanıcı Adı");
+        kullanıcı.setPreferredSize(new Dimension(200,40));
+
+        panelust.add(üstlabel);
+        panelust.add(kullanıcı);
+
         frame.setTitle("Giriş Sayfası");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400,300);
         frame.setLayout(new GridLayout(3,1));
+
+        frame.add(panelust);
         frame.setVisible(true);
     }
 }
